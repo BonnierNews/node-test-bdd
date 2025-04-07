907c19d52bd90261745299d4fd877f886ec87cbe
kristofferjansson <kristoffer.jansson@bonniernews.se>
2025-04-07 15:36:10 +0200

    Add support for .skip on all test functions

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ function Feature(text, testFn) {
   });
 }
 
+Feature.skip = function (text, testFn) {
+  return describe.skip(`Scenario: ${text}`, () => {
+    testFn();
+  });
+};
+
 function Scenario(text, testFn) {
   const beforeHook = featureStorage.getStore().get(`${executionAsyncId()}_before`);
   const afterHook = featureStorage.getStore().get(`${executionAsyncId()}_after`);
@@ -33,25 +39,51 @@ function Scenario(text, testFn) {
   });
 }
 
+Scenario.skip = function (text, testFn) {
+  return describe.skip(`Scenario: ${text}`, () => {
+    testFn();
+  });
+};
+
 function Given(text, testFn) {
   return it(`Given ${text}`, testFn);
 }
+
+Given.skip = function (text, testFn) {
+  return it.skip(`Given ${text}`, testFn);
+};
 
 function When(text, testFn) {
   return it(`When ${text}`, testFn);
 }
 
+When.skip = function (text, testFn) {
+  return it.skip(`When ${text}`, testFn);
+};
+
 function Then(text, testFn) {
   return it(`Then ${text}`, testFn);
 }
+
+Then.skip = function (text, testFn) {
+  return it.skip(`Then ${text}`, testFn);
+};
 
 function And(text, testFn) {
   return it(`And ${text}`, testFn);
 }
 
+And.skip = function (text, testFn) {
+  return it.skip(`And ${text}`, testFn);
+};
+
 function But(text, testFn) {
   return it(`But ${text}`, testFn);
 }
+
+But.skip = function (text, testFn) {
+  return it.skip(`But ${text}`, testFn);
+};
 
 function afterEachScenario(hookFn) {
   if (featureStorage.getStore()) {
