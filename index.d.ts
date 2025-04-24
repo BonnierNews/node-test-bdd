33f54b5c74a3ff610770aed73fac3fecb5850963
kristofferjansson <kristoffer.jansson@bonniernews.se>
2025-04-24 15:28:15 +0200

    Add type definitions

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+interface NodeTestBDDFunction {
+  (description: string, testFn: Function): any;
+}
+
+interface NodeTestBDDHook {
+  (callback: Function): void;
+  (description: string, callback: Function): void;
+}
+
+declare let Feature: NodeTestBDDFunction;
+declare let Scenario: NodeTestBDDFunction;
+declare let Given: NodeTestBDDFunction;
+declare let And: NodeTestBDDFunction;
+declare let When: NodeTestBDDFunction;
+declare let Then: NodeTestBDDFunction;
+declare let But: NodeTestBDDFunction;
+
+declare let afterEachFeature: NodeTestBDDHook;
+declare let beforeEachFeature: NodeTestBDDHook;
+declare let afterEachScenario: NodeTestBDDHook;
+declare let beforeEachScenario: NodeTestBDDHook;
